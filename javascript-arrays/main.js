7e3960166a2750e442132e4ee02d9f21df976258
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-10 15:24:18 -0700

    Victor Kalinkin javascript-arrays (initial submission)

---
+++ javascript-arrays/main.js
@@ -0,0 +1,19 @@
+var colors = ['red', 'white', 'blue'];
+console.log('Colors Array:', colors);
+console.log('Colors array position 0:', colors[0]);
+console.log('Colors array position 1:', colors[1]);
+console.log('Colors array position 2:', colors[2]);
+console.log('The American flag is ', colors[0] + ', ' + colors[1] + ', and ' +
+colors[2]);
+colors[2] = 'green';
+console.log('The Mexican flag is ', colors[0] + ', ' + colors[1] + ', and ' +
+  colors[2]);
+console.log('Current Colors Array:', colors);
+
+var students = ['Harry', 'Ron', 'Hermione', 'Draco'];
+var numberOfStudents = students.length;
+console.log('The class has ' + numberOfStudents + ' students');
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('The last student in the class is ' + lastStudent);
+console.log('Students Array (all of the students):', students);
