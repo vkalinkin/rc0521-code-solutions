66bdd08062cb236fc13ba9579997e60f1d0fc7f9
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-10 10:36:34 -0700

    Victor Kalinkin javascript-primitives-and-variables (initial submission)

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Victor Kalinkin';
+var isCool = true;
+var totalPets = 0;
+console.log('Full Name:', fullName);
+console.log('Is cool?:', isCool);
+console.log('Total Pets:', totalPets);
+console.log('Type of fullName:', typeof fullName);
+console.log('Type of isCool:', typeof isCool);
+console.log('Type of totalPets:', typeof totalPets);
+var unknown = null;
+console.log('Unkown:', unknown);
+console.log('Type of Unknown:', typeof unknown);
+var never;
+console.log('Never:', never);
+console.log('Type of Never:', typeof never);
