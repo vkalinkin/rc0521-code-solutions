f4f81d023fbf30073fbf4ad619e23f8f67943e71
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-10 11:56:23 -0700

    Victor Kalinkin javascript-operators (initial sumbission)

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 120;
+var height = 50;
+var area = width * height;
+console.log('Area:', area);
+console.log('Type of Area:', typeof area);
+
+var bill = 20;
+var payment = 22;
+var change = payment - bill;
+console.log('Change:', change);
+console.log('Type of Change:', typeof change);
+
+var quizzes = 86;
+var midterm = 72;
+var final = 95;
+var grade = (final + midterm + quizzes) / 3;
+console.log('Grade:', grade);
+console.log('Type of Grade:', typeof grade);
+
+var firstName = 'Victor';
+var lastName = 'Kalinkin';
+var fullName = firstName + ' ' + lastName;
+console.log('Full Name:', fullName);
+console.log('Type of Full Name:', typeof fullName);
+
+var pH = 9;
+var isAcidic = pH < 7;
+console.log('Is Acidic?', isAcidic);
+console.log('Type of Is Acidic:', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+console.log('Is Sparta?', isSparta);
+console.log('isSparta type:', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('Motto:', motto);
+console.log('Motto type:', typeof motto);
