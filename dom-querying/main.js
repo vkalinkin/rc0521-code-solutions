ad44ae43d8514982ab51559b1bdf88dfe69a3b63
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-17 10:35:52 -0700

    Victor K initial submission

---
+++ dom-querying/main.js
@@ -0,0 +1,22 @@
+// console.log('hello, word');
+// function printHello (){
+//   console.log('hey');
+// }
+
+var h1 = document.querySelector('h1');
+console.log('h1', h1);
+
+// console.log(document.querySelector('h1'));
+// console.dir(h1);
+
+var explanation = document.getElementById('explanation');
+console.dir(explanation);
+
+var hint = document.getElementsByClassName('hint');
+console.dir(hint);
+
+var p = document.querySelectorAll('p');
+console.dir(p);
+
+var exampleLink = document.getElementsByClassName('example-link');
+console.dir(exampleLink);
