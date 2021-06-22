8aac39788200bc965cbcfd2b3ecc6b4b32e550c6
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-22 14:46:59 -0700

    Victor K node-module-system (1st submission)

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,18 @@
+console.log('Type of Exports:', typeof (exports));
+console.log('Content of Exports:', exports);
+console.log('-------------------------------------------');
+
+console.log('Type of Require:', typeof (require));
+console.log('Contents of Require:', require);
+console.log('-------------------------------------------');
+
+console.log('Type of Module:', typeof (module));
+console.log('Contents of Module:', module);
+console.log('-------------------------------------------');
+
+console.log('Type of __Filename:', typeof (__filename));
+console.log('Contents of __Filename:', __filename);
+console.log('-------------------------------------------');
+
+console.log('Type of __Dirname:', typeof (__dirname));
+console.log('Contents of __Dirname:', __dirname);
