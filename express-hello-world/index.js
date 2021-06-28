85d2afafbc0715e8eef39ece67c188eb27b5de5c
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-28 11:48:19 -0700

    Victor K express-hello-world (2nd submission)

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(function (req, res) {
-  // console.log('Method property of req:', req.method);
-  // console.log('Send method of res:', res.send('Hello world!'));
+  // eslint-disable-next-line no-console
+  console.log('Method property of req:', req.method);
+  const sentMessage = res.send('Hello World');
+  // eslint-disable-next-line no-console
+  console.log('Send method of res:', sentMessage);
 });
 
 app.listen(3000, () => {
-  // eslint-diable-next line no-console
-  // console.log('Express server listening on port 3000');
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
 });
