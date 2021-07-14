506e89e5529e167086fa87e888db75d92e118562
Victor Kalinkin <vakalinkin@hotmail.com>
2021-07-13 19:01:21 -0700

    Victor K validated-input (changed classnames on icon elements and combined setstate calls)

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -3,21 +3,27 @@ import React from 'react';
 class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '', message: 'A password is required.', symbol: 'fas fa-times' };
+    this.state = { value: '', message: 'A password is required.', symbol: 'red fas fa-times' };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
     if (event.target.value.length === 0) {
-      this.setState({ message: 'A password is required.' });
-      this.setState({ symbol: 'fas fa-times' });
+      this.setState({
+        message: 'A password is required.',
+        symbol: 'red fas fa-times'
+      });
     } else if (event.target.value.length > 0 && event.target.value.length < 8) {
-      this.setState({ message: 'Your password is too short' });
-      this.setState({ symbol: 'fas fa-times' });
+      this.setState({
+        message: 'Your password is too short',
+        symbol: 'red fas fa-times'
+      });
     } else if (event.target.value.length >= 8) {
-      this.setState({ message: '' });
-      this.setState({ symbol: 'fas fa-check' });
+      this.setState({
+        message: '',
+        symbol: 'green fas fa-check'
+      });
     }
   }
 
