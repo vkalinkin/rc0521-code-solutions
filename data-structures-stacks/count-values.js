5137d4cc217aae57afbdfb454853c866df5bf567
Victor Kalinkin <vakalinkin@hotmail.com>
2021-08-03 10:51:40 -0700

    Victor K data-structrues-stacks (optimized max-value and count-value)

--- data-structures-stacks/count-values.js
+++ data-structures-stacks/count-values.js
@@ -3,15 +3,22 @@
 function countValues(stack) {
   let count = 0;
   let topValue;
-  topValue = stack.peek();
-  if (topValue === undefined) {
-    return count;
-  } else {
-    while (topValue !== undefined) {
-      stack.pop();
-      topValue = stack.peek();
-      count++;
-    }
-    return count;
+  topValue = stack.pop();
+  // if (topValue === undefined) {
+  //   return count;
+  // } else {
+  //   while (topValue !== undefined) {
+  //     stack.pop();
+  //     topValue = stack.peek();
+  //     count++;
+  //   }
+  //   return count;
+  // }
+  while (topValue !== undefined) {
+    // stack.pop();
+    // topValue = stack.peek();
+    topValue = stack.pop();
+    count++;
   }
+  return count;
 }
--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,17 +1,32 @@
 /* exported maxValue */
 
 function maxValue(stack) {
-  let maxValue;
+  // let maxValue;
+  // let topValue;
+  // topValue = stack.peek();
+  // if (topValue === undefined) {
+  //   return -Infinity;
+  // } else {
+  //   while (topValue !== undefined) {
+  //     const currentValue = stack.pop();
+  //     if (maxValue === undefined) {
+  //       maxValue = currentValue;
+  //     } else if (currentValue > maxValue) {
+  //       maxValue = currentValue;
+  //     }
+  //     topValue = stack.peek();
+  //   }
+  //   return maxValue;
+  // }
+  let maxValue = -Infinity;
   let topValue;
   topValue = stack.peek();
   if (topValue === undefined) {
-    return -Infinity;
+    return maxValue;
   } else {
     while (topValue !== undefined) {
       const currentValue = stack.pop();
-      if (maxValue === undefined) {
-        maxValue = currentValue;
-      } else if (currentValue > maxValue) {
+      if (currentValue > maxValue) {
         maxValue = currentValue;
       }
       topValue = stack.peek();
