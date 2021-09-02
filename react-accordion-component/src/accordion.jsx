fdcb7677fbc773f1bda201d770833a0e37f611d4
Victor Kalinkin <vakalinkin@hotmail.com>
2021-09-02 16:15:52 -0700

    testing handleClick and custom attributes

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -26,20 +26,37 @@ class Accordion extends React.Component {
       ]
     };
 
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(event) {
+    // console.log('heading clicked!');
+    // console.log(event.target);
+
+    // eslint-disable-next-line no-console
+    console.log('topicid: ', event.target.getAttribute('topicid'));
+    // console.log('header: ', event.target.getAttribute('header'));
+    // console.log('open: ', event.target.getAttribute('open'));
+    // const targetHeaderId = event.target.getAttribute('topicid');
+    // const targetOpen = event.target.getAttribute('open');
+    // console.log("0: ", this.state);
+
   }
 
   render() {
     const topicsArray = this.state.topics;
     return <div>
-      <div>Accordion:</div>
-      <div></div>
-        {
-          topicsArray.map((topic, index) => {
-            return <Topic key={index} topicId = {topic.topicId} header= {topic.header} details={topic.details} open = {topic.open} handleClick = {this.handleClick}/>;
-          })
-        }
+      <div>Test2</div>
+      <div id="accordion"></div>
+      {
+        topicsArray.map((topic, index) => {
+          return <Topic key={index} topicId={topic.topicId} header={topic.header} details={topic.details} open={topic.open} handleClick={this.handleClick} />;
+        })
+      }
     </div>;
   }
+
 }
 
 class Topic extends React.Component {
@@ -47,7 +64,7 @@ class Topic extends React.Component {
     // console.log("props:", this.props);
     return <div className="topic">
       <div>{this.props.topicId}</div>
-      <div>{this.props.header}</div>
+      <h2 onClick={this.props.handleClick} topicid={this.props.topicId} header={this.props.header} open={this.props.open}> {this.props.header}</h2>
       <div className={this.props.open ? 'content open' : 'content closed'}>
         {this.props.details}
       </div>
