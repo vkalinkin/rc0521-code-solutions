c801caece3e815ebd18dd2f26cdd77037ea89186
Victor Kalinkin <vakalinkin@hotmail.com>
2021-09-03 16:53:22 -0700

    completed accordion functionality

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,26 +4,7 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      topics: [
-        {
-          topicId: 1,
-          header: 'Hypertest Markup Language',
-          details: 'html descript',
-          open: true
-        },
-        {
-          topicId: 2,
-          header: 'Cascading Style Sheets',
-          details: 'css descrip',
-          open: false
-        },
-        {
-          topicId: 3,
-          header: 'JavaScript',
-          details: 'js descrip',
-          open: false
-        }
-      ]
+      openId: 0
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -31,41 +12,38 @@ class Accordion extends React.Component {
   }
 
   handleClick(event) {
-    // console.log('heading clicked!');
-    // console.log(event.target);
-
-    // eslint-disable-next-line no-console
-    console.log('topicid: ', event.target.getAttribute('topicid'));
-    // console.log('header: ', event.target.getAttribute('header'));
-    // console.log('open: ', event.target.getAttribute('open'));
-    // const targetHeaderId = event.target.getAttribute('topicid');
-    // const targetOpen = event.target.getAttribute('open');
-    // console.log("0: ", this.state);
+    const targetId = event.target.getAttribute('topicid');
+    const currentId = this.state.openId;
+    if (targetId === currentId) {
+      this.setState({ openId: 0 });
+    } else {
+      this.setState({ openId: targetId });
+    }
 
   }
 
   render() {
-    const topicsArray = this.state.topics;
+    const topicsArray = this.props.topicInfo;
     return <div>
       <div>Test2</div>
       <div id="accordion"></div>
       {
         topicsArray.map((topic, index) => {
-          return <Topic key={index} topicId={topic.topicId} header={topic.header} details={topic.details} open={topic.open} handleClick={this.handleClick} />;
+          return <Topic key={index} topicId={topic.topicId} header={topic.header} details={topic.details} open={topic.open} handleClick={this.handleClick} openId={this.state.openId}/>;
         })
       }
     </div>;
   }
-
 }
 
 class Topic extends React.Component {
+
   render() {
-    // console.log("props:", this.props);
+
     return <div className="topic">
       <div>{this.props.topicId}</div>
       <h2 onClick={this.props.handleClick} topicid={this.props.topicId} header={this.props.header} open={this.props.open}> {this.props.header}</h2>
-      <div className={this.props.open ? 'content open' : 'content closed'}>
+      <div className={this.props.topicId === parseInt(this.props.openId) ? 'content open' : 'content closed'}>
         {this.props.details}
       </div>
     </div>;
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const topicInfo = [
+  {
+    topicId: 1,
+    header: 'Hypertest Markup Language',
+    details: 'html descript',
+    open: true
+  },
+  {
+    topicId: 2,
+    header: 'Cascading Style Sheets',
+    details: 'css descrip',
+    open: false
+  },
+  {
+    topicId: 3,
+    header: 'JavaScript',
+    details: 'js descrip',
+    open: false
+  }
+];
+
 ReactDOM.render(
-  <Accordion/>,
+  <Accordion topicInfo={topicInfo}/>,
   document.querySelector('#root')
 );
