3e9b7d489ab910a6e6e341ee4de4f574c9d7b2e2
Victor Kalinkin <vakalinkin@hotmail.com>
2021-09-28 12:21:36 -0700

    first 8 queue problems completed

---
+++ data-structures-queues/take-2nd.js
@@ -0,0 +1,20 @@
+/* exported take2nd */
+
+function take2nd(queue) {
+  let topValue;
+  topValue = queue.peek();
+
+  if (topValue === undefined) {
+    return undefined;
+  } else {
+    topValue = queue.dequeue();
+    const secondValue = queue.peek();
+    if (secondValue === undefined) {
+      return topValue;
+    } else {
+      queue.dequeue();
+      queue.enqueue(topValue);
+      return secondValue;
+    }
+  }
+}
