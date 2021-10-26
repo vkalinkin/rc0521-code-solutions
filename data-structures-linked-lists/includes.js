99699b97261747bf0d7313b7aa7f5617ad18f296
Victor Kalinkin <vakalinkin@hotmail.com>
2021-10-26 10:57:13 -0700

    Victor K completed linked lists

--- data-structures-linked-lists/includes.js
+++ data-structures-linked-lists/includes.js
@@ -1,5 +1,14 @@
 /* exported includes */
 
 function includes(list, value) {
+  let hasValue = false;
+  let currentVal = list;
+  while (currentVal !== null) {
 
+    if (currentVal.data === value) {
+      hasValue = true;
+    }
+    currentVal = currentVal.next;
+  }
+  return hasValue;
 }
--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -1,5 +1,12 @@
 /* exported removeTail */
 
 function removeTail(list) {
-
+  if (list.next !== null) {
+    let currentVal = list;
+    while (currentVal.next.next !== null) {
+      currentVal = currentVal.next.next;
+    }
+    currentVal.next = null;
+  }
+  return list;
 }
